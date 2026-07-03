abe011ad656c471585b1e8121c6cd8eb59cd2db7
Anhmy27 <myanhva@gmail.com>
2026-07-03 17:41:24 +0700

    fix

--- frontendnext/next.config.ts
+++ frontendnext/next.config.ts
@@ -4,7 +4,13 @@ const nextConfig: NextConfig = {
   experimental: {
     externalDir: true,
   },
-  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.111.41"],
+  allowedDevOrigins: [
+    "localhost",
+    "127.0.0.1",
+    "192.168.111.41",
+    "anhmy.online",
+    "www.anhmy.online",
+  ],
 };
 
 export default nextConfig;
